792b45aa8bd673d34c6a427b6c2a482875feda4e
TheJuanRojasR <juan_mrojasr@soy.sena.edu.co>
2025-11-10 12:14:25 -0500

    TypeScript Platzi - Habla sobre el "problema" que se tiene cuando cuando una funcion puede retornar multiples tipos de datos y toca hacer una asersion para que TS permite hacer algunas acciones.
    Por cada video tomare nota y creare un commit para tener todo organizado.

---
+++ src/01_Platzi/02_TiposAvanzados_Funciones/10_overload.ts
@@ -0,0 +1,32 @@
+// 10.11.25
+
+// Clase 10
+// Sobrecarga de funciones (Problema): TypeScript no puede inferir que tipo de dato retornara una funcion cuando esta puede retornar multiples TiposDeDatos
+// NOTA: La sobrecarga solo funciona con funciones declarativas (function)
+
+// Entrada: Nico | Salida: [N, i, c, o] => string | string[]
+// Entrada: [N, i, c, o] | Salida: Nico => string[] | string
+
+function parseStr (input: string | string[]): string | string[] {
+  if (Array.isArray(input)) return input.join('');
+
+  return input.split('');
+}
+
+const responseArray = parseStr("Nico");
+console.log("responseArray: " + responseArray);
+
+// Como TS con respuesto al argumento que devolvera es por esa razon que tenemos que inferir asersion de tipo (Decirle de forma explicita el TipoDeDato a TS)
+if (Array.isArray(responseArray)) {
+  console.log(responseArray.reverse());
+}
+
+const responseString = parseStr(["N", "I", "C", "O"]);
+console.log("responseString: " + responseString);
+
+
+if (typeof responseString === 'string') {
+  console.log(responseString.toLocaleLowerCase());
+}
+
+
