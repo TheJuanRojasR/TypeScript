be2d293a15518a0cad6dc1874a60d9beb092f6e2
TheJuanRojasR <juan_mrojasr@soy.sena.edu.co>
2025-11-13 10:06:09 -0500

    TypeScript Platzi: En la clase 15 se habla sobre la propiedad readonly.
    Esta propiedad ayuda a que una variable solo sea de lectura y no se pueda modificar

--- src/01_Platzi/02_TiposAvanzados_Funciones/01_App/base.model.ts
+++ src/01_Platzi/02_TiposAvanzados_Funciones/01_App/base.model.ts
@@ -4,9 +4,12 @@
 // Extender interfaces (herencia):
 // Se genera una interface base con las propiedades en comun que tienen todos los otros modelos y despues se extienden a los demas modelos.
 
+// Clase 15
+// readonly : Utilizamos la propiedad readonly para que sea solo de lectura y nadie la pueda sobreescribir
+
 // Si quisiera que el id fuera string solo lo cambio aca y no en todos los modelos.
 export interface BaseModelInterface {
-  id: string | number;
-  createdAt: Date;
+  readonly id: string | number;
+  readonly createdAt: Date;
   updatedAt: Date;
 }
